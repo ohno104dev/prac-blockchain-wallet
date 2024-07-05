ad0d82af915d491167ad73e89cdb9064f75d727e
ohno <ohno104dev@gmail.com>
2024-07-05 22:30:48 +0800

    feat: add server providing user interface to wallets

---
+++ wallet_server/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"flag"
+	"log"
+)
+
+func Init() {
+	log.SetPrefix("Wallet Server: ")
+}
+
+func main() {
+	port := flag.Uint("port", 8080, "TCP Number for Online Wallet")
+	gateway := flag.String("gateway", "http://127.0.0.1:3333", "Blockchain Gateway")
+	flag.Parse()
+
+	app := NewWalletServer(uint16(*port), *gateway)
+	log.Println("Starting wallet server on port:", *port, "using blockchain node", *gateway, "as gateway")
+	app.Run()
+}
---
+++ wallet_server/wallet_server.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"path"
+	"strconv"
+	"text/template"
+)
+
+const pathToTemplateDir = "templates"
+
+type WalletServer struct {
+	port    uint16
+	gateway string
+}
+
+func NewWalletServer(port uint16, gateway string) *WalletServer {
+	return &WalletServer{port, gateway}
+}
+
+func (ws *WalletServer) Port() uint16 {
+	return ws.port
+}
+
+func (ws *WalletServer) Gateway() string {
+	return ws.gateway
+}
+
+func (ws *WalletServer) Index(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case http.MethodGet:
+		t, _ := template.ParseFiles(path.Join(pathToTemplateDir, "index.html"))
+		t.Execute(w, "")
+	default:
+		log.Printf("ERROR: Invalid HTTP method")
+	}
+}
+
+func (ws *WalletServer) Run() {
+	http.HandleFunc("/", ws.Index)
+	log.Fatal(http.ListenAndServe("0.0.0.0:"+strconv.Itoa(int(ws.port)), nil))
+}
